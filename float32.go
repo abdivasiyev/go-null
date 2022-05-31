25e2d7b1d08ad166fa52be2803e41d509bd891b3
Asliddin Azizovich <asliddin.abdivasiyev@gmail.com>
2022-05-31 16:31:00 +0500

    feat: float32 scanner and marshaller

---
+++ float32.go
@@ -0,0 +1,76 @@
+package null
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
+type Float32 struct {
+	nullValue *sql.NullFloat64
+}
+
+func (s Float32) String() string {
+	if s.nullValue != nil {
+		return fmt.Sprint(s.nullValue.Float64)
+	}
+
+	return ""
+}
+
+func NewFloat32(value float32) Float32 {
+	return Float32{
+		nullValue: &sql.NullFloat64{
+			Valid:   true,
+			Float64: float64(value),
+		},
+	}
+}
+
+func (s Float32) Get() float32 {
+	if s.nullValue != nil {
+		return float32(s.nullValue.Float64)
+	}
+
+	return 0
+}
+
+func (s *Float32) Value() (driver.Value, error) {
+	if s.nullValue != nil {
+		return s.nullValue.Value()
+	}
+
+	return nil, nil
+}
+
+func (s *Float32) Scan(value any) error {
+	if err := s.nullValue.Scan(value); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Float32) UnmarshalJSON(bytes []byte) error {
+	s.nullValue = &sql.NullFloat64{
+		Float64: 0,
+		Valid:   false,
+	}
+
+	if len(bytes) > 0 {
+		if err := json.Unmarshal(bytes, &s.nullValue.Float64); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s Float32) MarshalJSON() ([]byte, error) {
+	if s.nullValue.Valid {
+		return json.Marshal(s.nullValue.Float64)
+	}
+
+	return json.Marshal(nil)
+}
---
+++ float32_test.go
@@ -0,0 +1,33 @@
+package null
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestFloat32(t *testing.T) {
+	testCases := []struct {
+		name string
+		s    Float32
+	}{
+		{
+			name: "Not null marshaller",
+			s:    NewFloat32(3.2),
+		},
+		{
+			name: "null marshaller",
+			s:    NewFloat32(0),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := json.Marshal(tc.s)
+
+			if err != nil {
+				t.Errorf("could not marshal: %v\n", err)
+				t.FailNow()
+			}
+		})
+	}
+}
