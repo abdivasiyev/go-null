566ede3d851652be2b9ddcd16fd79cbc7c46ef04
Asliddin Azizovich <asliddin.abdivasiyev@gmail.com>
2022-05-31 16:09:10 +0500

    feat: int32 scanner and marshaller

---
+++ int32.go
@@ -0,0 +1,76 @@
+package null
+
+import (
+	"database/sql"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
+type Int32 struct {
+	nullValue *sql.NullInt32
+}
+
+func (s Int32) String() string {
+	if s.nullValue != nil {
+		return fmt.Sprint(s.nullValue.Int32)
+	}
+
+	return ""
+}
+
+func NewInt32(value int32) Int32 {
+	return Int32{
+		nullValue: &sql.NullInt32{
+			Valid: true,
+			Int32: value,
+		},
+	}
+}
+
+func (s Int32) Get() int32 {
+	if s.nullValue != nil {
+		return s.nullValue.Int32
+	}
+
+	return 0
+}
+
+func (s *Int32) Value() (driver.Value, error) {
+	if s.nullValue != nil {
+		return s.nullValue.Value()
+	}
+
+	return nil, nil
+}
+
+func (s *Int32) Scan(value any) error {
+	if err := s.nullValue.Scan(value); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Int32) UnmarshalJSON(bytes []byte) error {
+	s.nullValue = &sql.NullInt32{
+		Int32: 0,
+		Valid: false,
+	}
+
+	if len(bytes) > 0 {
+		if err := json.Unmarshal(bytes, &s.nullValue.Int32); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s Int32) MarshalJSON() ([]byte, error) {
+	if s.nullValue.Valid {
+		return json.Marshal(s.nullValue.Int32)
+	}
+
+	return json.Marshal(nil)
+}
---
+++ int32_test.go
@@ -0,0 +1,42 @@
+package null
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+func TestInt32(t *testing.T) {
+	testCases := []struct {
+		name            string
+		s               Int32
+		marshalledBytes []byte
+	}{
+		{
+			name:            "Not null marshaller",
+			s:               NewInt32(1),
+			marshalledBytes: []byte(`1`),
+		},
+		{
+			name:            "null marshaller",
+			s:               NewInt32(0),
+			marshalledBytes: []byte(`0`),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			bytes, err := json.Marshal(tc.s)
+
+			if err != nil {
+				t.Errorf("could not marshal: %v\n", err)
+				t.FailNow()
+			}
+
+			if !reflect.DeepEqual(bytes, tc.marshalledBytes) {
+				t.Errorf("not equal values => got: %v, want: %v\n", string(bytes), string(tc.marshalledBytes))
+				t.FailNow()
+			}
+		})
+	}
+}
